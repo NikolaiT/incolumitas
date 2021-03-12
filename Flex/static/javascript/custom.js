2e42ad81a78f5a4f4a29b45245ffce808e72e076
Nikolai Tschacher <nikolai.tschacher@informatik.hu-berlin.de>
2021-03-12 22:07:19 +0100

    large img on click

--- Flex/static/javascript/custom.js
+++ Flex/static/javascript/custom.js
@@ -1,10 +1,12 @@
 
-var imgs = document.querySelectorAll('.post-content img');
+document.addEventListener('load', function() {
+    var imgs = document.querySelectorAll('.post-content img');
 
-function fullwidth(event) {
-  window.location = event.target.src;
-}
+    function fullwidth(event) {
+        window.location = event.target.src;
+    }
 
-imgs.forEach((node) => {
-  node.addEventListener('click', fullwidth);
+    imgs.forEach((node) => {
+        node.addEventListener('click', fullwidth);
+    });
 });
