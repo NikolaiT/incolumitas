499ef5971fc79699a11bf0ad964dc0c5a9855964
Nikolai Tschacher <admin@incolumitas.com>
2023-03-28 20:55:15 +0200

    auto commit (I am lazy)

--- publish.sh
+++ publish.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 
-PRIV_KEY=~/.ssh/root_new_server
-pelican -s publishconf.py
+PRIV_KEY=~/.ssh/root_new_server;
 
-# function deploy_isso () {
-#     scp -i $PRIV_KEY /home/nikolai/projects/work/backups/var/lib/isso/comments.db root@167.99.241.135:/var/lib/isso/comments.db;
-#     scp -i $PRIV_KEY /home/nikolai/projects/private/incolumitas/incolumitas/isso/isso.cfg root@167.99.241.135:/etc/isso.cfg;
+if [[ $1 = "build" ]]; then
+  pelican -s publishconf.py;
+fi
 
-#     scp -i $PRIV_KEY /home/nikolai/projects/private/incolumitas/incolumitas/isso/isso_nginx.conf root@167.99.241.135:/etc/nginx/sites-available/isso_nginx.conf;
+function deploy_isso () {
+  scp -i $PRIV_KEY /home/nikolai/projects/work/backups/var/lib/isso/comments.db root@167.99.241.135:/var/lib/isso/comments.db;
+  scp -i $PRIV_KEY /home/nikolai/projects/private/incolumitas/incolumitas/isso/isso.cfg root@167.99.241.135:/etc/isso.cfg;
+  scp -i $PRIV_KEY /home/nikolai/projects/private/incolumitas/incolumitas/isso/isso_nginx.conf root@167.99.241.135:/etc/nginx/sites-available/isso_nginx.conf;
+  scp -i $PRIV_KEY /home/nikolai/projects/private/incolumitas/incolumitas/isso/isso.service root@167.99.241.135:/etc/systemd/system/isso.service;
+}
 
-#     # ssh -i $PRIV_KEY root@167.99.241.135 "ln -s /etc/nginx/sites-available/isso_nginx.conf /etc/nginx/sites-enabled/isso_nginx.conf"
-
-#     scp -i $PRIV_KEY /home/nikolai/projects/private/incolumitas/incolumitas/isso/isso.service root@167.99.241.135:/etc/systemd/system/isso.service;
-#     reload_isso();
-# }
+if [[ $1 = "isso" ]]; then
+  deploy_isso;
+fi
 
 echo "Upload blog contents"
-rsync -avc --delete -e "ssh -i $PRIV_KEY" output/ root@167.99.241.135:/var/www/incolumitas.com/
+rsync -q -avc --delete -e "ssh -i $PRIV_KEY" output/ root@167.99.241.135:/var/www/incolumitas.com/
 ssh -i $PRIV_KEY root@167.99.241.135 "chown -R www-data:www-data /var/www/incolumitas.com/"
 
 # echo "Restart server and isso"
